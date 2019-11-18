5b0d9a458c20cabd753b53b7f792e9c9a4c6435d
nzandrewahn <nzandrewahn@gmail.com>
2019-11-18 16:19:22 +1300

    First commit

---
+++ script.js
@@ -0,0 +1,89 @@
+
+let buttonList = document.querySelectorAll("button");
+let output = document.querySelector("#output");
+
+console.log(buttonList);
+let clicks = 0;
+let operator = 0;
+let printValue; 
+let valueOne, valueTwo;
+
+
+for (let i = 0; i < buttonList.length; i++) {
+    buttonList[i].onclick = function () {
+        let input = buttonList[i].innerHTML
+
+        if (isNaN(input)) {
+            operator = input;
+            printValue += operator;
+            output.value = printValue;
+            clicks = 0;
+            return;
+        }
+
+        if (input == 'exp'){
+            console.log('YEEEEEEEEET');
+            return;
+        }
+
+        if (!operator) {
+            //If Number and a operator hasnt been pressed yet
+            if (clicks > 0) {
+                valueOne = valueOne + input;
+            } else {
+                valueOne = input;
+            }
+            printValue = valueOne;
+        } else {
+            //If Number and operator has been pressed need to assign to ValueTwo;
+            if (clicks > 0) {
+                valueTwo = valueTwo + input;
+            } else {
+                valueTwo = input;
+            }
+            printValue += input;
+        }
+        clicks++;
+
+        output.value = printValue;
+
+        console.log('valueOne is: ' + valueOne);
+        console.log('valueTwo is: ' + valueTwo);
+        console.log('PrintValue is: ' + printValue);
+
+
+    }
+}
+
+
+let enterButton = document.querySelector('#enter');
+let result;
+
+enterButton.onclick = function() {
+    switch (operator) {
+        case '+': 
+        console.log('HELLOOO');
+            result = parseInt(valueOne) + parseInt(valueTwo);
+        break;
+
+        case '-': 
+        result = parseInt(valueOne) - parseInt(valueTwo);
+        break;
+
+        case '*': 
+        result = parseInt(valueOne) * parseInt(valueTwo);
+        break;
+
+        case '/': 
+        result = parseInt(valueOne) / parseInt(valueTwo);
+        break;
+    }
+
+    output.value = result;
+};
+
+
+
+
+
+//Firstly get the numbers when pressed and display to the text -field
---
+++ test.js
@@ -0,0 +1,54 @@
+
+// // for (player of players){
+// //     let team = Math.floor((Math.random() * 6) + 1);
+// //     console.log("The team is: " + team);
+// // }
+
+
+
+// // document.getElementById('button').addEventListener("click", function(event){
+// //     event.preventDefault();
+// //     console.log(document.getElementById("input").value);
+// // });
+
+
+// var players = ['Thomas', 'Joshua', 'Brian', 'Jackie'];
+// console.log(players);
+// console.log(players.sort());
+
+// var completed = [];
+// let i = 0;
+
+
+// var Player = { 
+//     name: 'Thomas',
+//     lastname: 'Yang',
+//     age: 20,
+//     friends: ['Helen', 'Dylan'],
+//     address: {
+//         street: 'Santa Ana Drive',
+//         city: 'Auckland', 
+//         suburb: 'Dannemora', 
+//     }
+// }
+
+// console.log(Player.name);
+// console.log(Player.age);
+// console.log(Player.friends[0]);
+
+// console.log(Player.address.suburb);
+
+
+
+// var Student = new Object();
+// Student.name = 'Andrew';
+
+// Student.describe = function(){
+//     return this.name;
+// }
+
+// console.log(Student.describe());
+
+
+
+
